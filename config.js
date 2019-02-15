73566b37bf7879ab5c86dcf2c18a4e964f86c47a
Amaury-Tobias@CSI <amaury.tobiasqr+csi@gmail.com>
2019-02-15 17:02:10 -0600

    vue start

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 8080,
-    db: 'mongodb://localhost:27017/chat',
-    SECRET_TOKEN: 'miclavetoken'
+  port: process.env.PORT || 8080,
+  db: 'mongodb://localhost:27017/chat',
+  SECRET_TOKEN: 'miclavetoken'
 }
